5ffe67da71911285f991578440aa5b759610771c
mahnoornav <142110353+mahnoornav@users.noreply.github.com>
2026-02-13 13:58:46 -0500

    final rode code

---
+++ Road.java
@@ -0,0 +1,18 @@
+public class Road {
+    private final Player owner;
+    private final int edge;
+
+    public Road(Player owner, int edge) {
+        this.owner = owner;
+        this.edge = edge;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public int getEdge() {
+        return edge;
+    }
+}
+
