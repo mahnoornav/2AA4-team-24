39db063c248d5974d50219906099f16b1f0f4e3b
mahnoornav <mahnoornaveed06@gmail.com>
2026-03-08 17:11:45 -0400

    added structure, settlement, and city classes, closes #34, #35

---
+++ Assignment_2/RevisedCatanCode/Settlement.java
@@ -0,0 +1,6 @@
+public class Settlement extends Structure {
+    
+    public Settlement(Player owner, int vertex) {
+        super(owner, vertex);
+    }
+}
---
+++ Assignment_2/RevisedCatanCode/Structure.java
@@ -0,0 +1,14 @@
+public abstract class Structure {
+    private Player owner;
+    private int vertex;
+
+    public Structure(Player player, int vertex) {
+        this.player = player;
+        this.vertex = vertex;
+
+    }
+
+    public Player getPlayer(){
+        return this.player;
+    }
+}
