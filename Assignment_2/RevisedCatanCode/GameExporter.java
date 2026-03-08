7a4eb2f63604aa0abdda0d43783900b4c4d42418
Zainab Mirza <zainab@Zainabs-MacBook-Pro-2.local>
2026-03-08 15:27:34 -0400

    Added GameExporter class for visualizer

---
+++ Assignment_2/RevisedCatanCode/GameExporter.java
@@ -0,0 +1,11 @@
+/**
+ * This exports the current game state from Board.java into a JSON file required for
+ * the visualizer so it can display the Catan Board correctly.
+ */
+
+public class GameExporter {
+
+    public static void export(Board board) {
+        // Add JSON export logic for visualizer below
+    }
+}
