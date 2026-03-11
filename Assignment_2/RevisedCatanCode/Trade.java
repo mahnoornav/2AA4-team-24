96776073c8af111241c86d16452ae9112108caba
Alyshba <alyshbaarshad@example.com>
2026-03-11 11:27:20 -0400

    Missing method added to Trade

--- Assignment_2/RevisedCatanCode/Trade.java
+++ Assignment_2/RevisedCatanCode/Trade.java
@@ -1,16 +1,19 @@
+import java.util.Collections;
 import java.util.Map;
 
 public interface Trade {
     
     // store info about the current trade
-    void proposeTrade(Player receivesTrade, Map<ResourceType, Integer> resourceRequested, Map<ResourceType, Integer> resourceOffered);
+    public void proposeTrade(Player recievesTrade, Map<ResourceType, Integer> resourceRequested, Map<ResourceType, Integer> resourceOffered);
 
-    void tradeBank(ResourceType offer, ResourceType receive);
+    public void receiveTrade(Player proposer, Map<ResourceType,Integer> requested, Map<ResourceType,Integer> offered);
 
-    void receiveTrade(Player proposer, Map<ResourceType, Integer> requested, Map<ResourceType, Integer> offered);
+    private void tradeBank(Player requestsTrade, ResourceType offer, ResourceType recieve) {
+    }
 
-    void accept();
+    private void accept(){
+    }
 
-    void reject();
-    
+    private void reject() {
+    }
 }
